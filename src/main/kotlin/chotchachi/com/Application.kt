c3960e7d5addef298e28ae58bc4d7ccc90ef10ad
Thanh Quang <thanhquang.stahli@gmail.com>
2022-11-15 09:24:54 +0700

    [QuangTT]: - update cron time - fix CountdownJob

--- src/main/kotlin/chotchachi/com/Application.kt
+++ src/main/kotlin/chotchachi/com/Application.kt
@@ -66,7 +66,7 @@ private fun startCountdownJob(jobSchedulerManager: JobSchedulerManager) {
         .withIdentity(jobId)
         .build()
 
-    val cronSchedule = CronScheduleBuilder.cronSchedule("0 32 16 * * ?")
+    val cronSchedule = CronScheduleBuilder.cronSchedule("0 30 9 * * ?")
 
     val trigger = TriggerBuilder.newTrigger()
         .withIdentity("triggerForCountdownJob")
--- src/main/kotlin/chotchachi/com/jobs/CountdownJob.kt
+++ src/main/kotlin/chotchachi/com/jobs/CountdownJob.kt
@@ -3,11 +3,9 @@ package chotchachi.com.jobs
 import chotchachi.com.network.NetworkService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
-import kotlinx.datetime.Clock
-import kotlinx.datetime.Instant
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.daysUntil
+import kotlinx.datetime.*
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.quartz.Job
@@ -17,32 +15,40 @@ class CountdownJob : Job, KoinComponent {
 
     private val service by inject<NetworkService>()
 
+    private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
     override fun execute(context: JobExecutionContext?) {
-        val now = Clock.System.now()
-        val timeZone = TimeZone.of("UTC+7")
+        val now = Clock.System.now().toLocalDateTime(TimeZone.of("UTC+7")).date
 
-        val noelDaysDiff = now.daysUntil(NOEL, timeZone)
-        val newYearDatsDiff = now.daysUntil(NEW_YEAR, timeZone)
-        val tetDaysDiff = now.daysUntil(TET_HOLIDAY, timeZone)
+        val noelDaysDiff = now.daysUntil(NOEL)
+        val newYearDaysDiff = now.daysUntil(NEW_YEAR)
+        val tetDaysDiff = now.daysUntil(TET_HOLIDAY)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            service.sendMessage(
-                botToken = "5606196893:AAGGNhIbdoBzGTnQKD_FtHnwiAwDOP2EnmM",
-                chatId = "-839298164",
-                text = """
+        val chatIds = listOf(
+            "-839298164",
+            "-1001623104529"
+        )
+
+        coroutineScope.launch {
+            chatIds.forEach { chatId ->
+                service.sendMessage(
+                    botToken = "5606196893:AAGGNhIbdoBzGTnQKD_FtHnwiAwDOP2EnmM",
+                    chatId = chatId,
+                    text = """
 *Countdown*
 -------------------
 Còn $noelDaysDiff ngày nữa là Noel.
-Còn $newYearDatsDiff ngày nữa là Tết Dương.
+Còn $newYearDaysDiff ngày nữa là Tết Dương.
 Còn $tetDaysDiff ngày nữa là Tết Âm.
-                """.trimIndent()
-            )
+                """
+                )
+            }
         }
     }
 
     companion object {
-        private val NOEL = Instant.parse("2022-12-25T00:00:00Z")
-        private val NEW_YEAR = Instant.parse("2023-01-01T00:00:00Z")
-        private val TET_HOLIDAY = Instant.parse("2023-01-22T00:00:00Z")
+        private val NOEL = LocalDate(2022, 12, 25)
+        private val NEW_YEAR = LocalDate(2023, 1, 1)
+        private val TET_HOLIDAY = LocalDate(2023, 1, 22)
     }
 }
